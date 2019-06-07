3e1b98ea487447de7348d7d835744269933d5323
Nicolas MEO <nicolas.meo@gmail.com>
2019-06-07 14:31:32 +0200

    Login form en cours d'implémentation

--- django-fundamentals-course/tictactoe/player/urls.py
+++ django-fundamentals-course/tictactoe/player/urls.py
@@ -1,7 +1,10 @@
-from django.urls import path
+from django.conf.urls import url
 from .views import home
+from django.contrib.auth.views import LoginView, LogoutView
 
 
 urlpatterns = [
-    path("home", home)
+    url(r'home$', home, name="player_home"),
+    url(r'login$', LoginView.as_view(template_name="player/login_form.html"), name="player_login"),
+    url(r'logout$', LogoutView.as_view(),name="player_logout")
     ]
--- django-fundamentals-course/tictactoe/tictactoe/views.py
+++ django-fundamentals-course/tictactoe/tictactoe/views.py
@@ -2,7 +2,18 @@
 Exemple de creation de vue par fonction
 """
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def welcome(request):
-    return render(request, 'tictactoe/welcome.html')
+    """
+    Page d'acceuil
+    La condition user.is_authenticated était utilisee avant le login_required
+    Les 2 sont laissés à des fins pédagogiques mais cette condition n'est plus 
+    utile avec ce code
+    """
+    if request.user.is_authenticated:
+        return redirect('player_home')
+    else:
+        return render(request, 'tictactoe/welcome.html')
